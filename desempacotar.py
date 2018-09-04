b7818e72f0af990a9edcdae76100044c1a1ba6db
Fogelman <ddfogelman@gmail.com>
2018-09-04 17:57:25 -0300

    Adicionando mudancas no recebimento dos pacotes

--- desempacotar.py
+++ desempacotar.py
@@ -59,7 +59,7 @@ def find_EOP_BUFFER(len_data, data):
     found_eop = False
     i = 0
     for i in range(len_data -2):
-        print("OK")
+        # print("OK")
         if data_list[i:i+3] == [190,186,218]:
             found_eop = True
 
