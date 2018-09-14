a02aeb972911b85d81f055c479e876dfee56ea2d
Fogelman <ddfogelman@gmail.com>
2018-09-14 17:28:13 -0300

    crc funcionando

---
+++ crc.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def crc16(data: bytes):
+    '''
+    CRC-16-ModBus Algorithm
+    '''
+    data = bytearray(data)
+    poly = 0xA001
+    crc = 0xFFFF
+    for b in data:
+        crc = crc ^ (0xFF & b)
+        for _ in range(0, 8):
+            if (crc & 0x0001):
+                crc = ((crc >> 1) & 0xFFFF) ^ poly
+            else:
+                crc = ((crc >> 1) & 0xFFFF)
+
+    return np.uint16(crc)
