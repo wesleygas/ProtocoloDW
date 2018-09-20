e68de5a9085315710309327240b6bac8142339a4
wesleygas <wesleygl16@gmail.com>
2018-09-20 14:49:41 -0300

    crc_fix

--- crc.py
+++ crc.py
@@ -2,6 +2,12 @@ import numpy as np
 from binascii import crc_hqx
 
 def crc16(data):
-    crc = crc_hqx(data,0xFFFF)
+    data_bytes = bytes(data)
+    crc = crc_hqx(data_bytes,0xFFFF)
     
     return crc
+
+print(format(crc16(bytes([254])),'#04X'))
+
+
+#print(format(crc_hqx(bytes([254]),0xFFFF),'#04X'))
