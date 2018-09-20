a640044dcd04b40a3267ef87a8f055c80af809dc
wesleygas <wesleygl16@gmail.com>
2018-09-20 14:44:14 -0300

    CRC

--- crc.py
+++ crc.py
@@ -1,18 +1,7 @@
 import numpy as np
+from binascii import crc_hqx
 
-def crc16(data: bytes):
-    '''
-    CRC-16-ModBus Algorithm
-    '''
-    data = bytearray(data)
-    poly = 0xA001
-    crc = 0xFFFF
-    for b in data:
-        crc = crc ^ (0xFF & b)
-        for _ in range(0, 8):
-            if (crc & 0x0001):
-                crc = ((crc >> 1) & 0xFFFF) ^ poly
-            else:
-                crc = ((crc >> 1) & 0xFFFF)
-
-    return np.uint16(crc)
+def crc16(data):
+    crc = crc_hqx(data,0xFFFF)
+    
+    return crc
